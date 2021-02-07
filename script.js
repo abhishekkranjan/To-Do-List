1a57005c87f9d603cb15a2fffa6215f36182852b
abhishekkranjan <64324753+abhishekkranjan@users.noreply.github.com>
2021-02-07 12:27:03 +0530

    added JS file to my project

---
+++ script.js
@@ -0,0 +1,86 @@
+var addButton = document.getElementById("add-button");
+function addToDoItem() {
+  alert("Add button clicked!");
+}
+addButton.addEventListener("click", addToDoItem);
+var clearButton = document.getElementById("clear-completed-button");
+function clearCompletedToDoItems(){
+  alert("Clear Completed clicked.....!");
+}
+clearButton.addEventListener("click", clearCompletedToDoItems);
+var emptyButton = document.getElementById("empty-button");
+function emptyList(){
+  alert("Empty List clicked.....!");
+}
+emptyButton.addEventListener("click", emptyList);
+var saveButton = document.getElementById("save-button");
+/*function saveList(){
+  alert("Save List clicked.....!");
+}*/
+function saveList() {
+  var toDos = [];
+
+  for (var i = 0; i < toDoList.children.length; i++) {
+      var toDo = toDoList.children.item(i);
+
+      var toDoInfo = {
+          "task": toDo.innerText,
+          "completed": toDo.classList.contains("completed")
+      };
+
+      toDos.push(toDoInfo);
+
+  }
+
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+saveButton.addEventListener("click", saveList);
+var toDoEntryBox = document.getElementById("todo-entry-box");
+var toDoList = document.getElementById("todo-list");
+function newToDoItem(itemText, completed) {
+    var toDoItem = document.createElement("li");
+    var toDoText = document.createTextNode(itemText);
+    toDoItem.appendChild(toDoText);
+
+    if (completed) {
+        toDoItem.classList.add("completed");
+    }
+
+    toDoList.appendChild(toDoItem);
+    toDoItem.addEventListener("dblclick", toggleToDoItemState);
+}
+function addToDoItem() {
+    var itemText = toDoEntryBox.value;
+    newToDoItem(itemText, false);
+}
+function toggleToDoItemState() {
+    if (this.classList.contains("completed")) {
+        this.classList.remove("completed");
+    } else {
+        this.classList.add("completed");
+    }
+}
+function clearCompletedToDoItems() {
+    var completedItems = toDoList.getElementsByClassName("completed");
+
+    while (completedItems.length > 0) {
+        completedItems.item(0).remove();
+    }
+}
+function emptyList() {
+    var toDoItems = toDoList.children;
+    while (toDoItems.length > 0) {
+        toDoItems.item(0).remove();
+    }
+}
+function loadList() {
+  if (localStorage.getItem("toDos") != null) {
+      var toDos = JSON.parse(localStorage.getItem("toDos"));
+
+      for (var i = 0; i < toDos.length; i++) {
+          var toDo = toDos[i];
+          newToDoItem(toDo.task, toDo.completed);
+      }
+  }
+}
+loadList();
